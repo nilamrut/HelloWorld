fef2403e17b427df81c71caca95efb9153608f59
nilamrut <76804542+nilamrut@users.noreply.github.com>
2021-02-23 10:04:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,8 @@
+public class HelloWorld {
 
-Hello !
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
